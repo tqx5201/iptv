af3b3ce1957c12459a08dab6b9908c0e2fab0fe0
天仙婆婆 <tqx5201@163.com>
2025-06-05 10:41:06 +0800

    Update fofa.sh

--- speedtest/fofa.sh
+++ speedtest/fofa.sh
@@ -260,7 +260,8 @@ rm -rf tmp1.txt tmp2.txt tmp3.txt
 echo "===============-合并所有城市的txt文件为:zubo_fofa.txt================="
 output_file="zubo_fofa.txt"
 for file in txt/fofa_*.txt;do
-     filename=$(basename "$file")
+     #filename=$(basename "$file")
+     filename=$(basename "$file" | sed 's/_/-/g')
      echo "$filename,#genre#" >> "$output_file"
      cat "$file" >> "$output_file"
      echo "" >> "$output_file"
