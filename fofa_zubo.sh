02348a768e9a625d4d62e07afcc3cdd988e4cbec
天仙婆婆 <tqx5201@163.com>
2025-06-10 07:03:27 +0800

    Update fofa_zubo.sh

--- fofa_zubo.sh
+++ fofa_zubo.sh
@@ -194,7 +194,7 @@ function get_zubo_ip(){
         "Jiangsu" "Zhejiang" "Anhui" "Fujian" "Jiangxi" "Shandong"
         "Henan" "Hubei" "Hunan" "Guangdong" "Guangxi" "Hainan"
         "Sichuan" "Guizhou" "Yunnan" "Xizang" "Shaanxi" "Gansu"
-        "Qinghai" "Neimenggu" "Ningxia" "Xinjiang Uygur"
+        "Qinghai" "Neimenggu" "Ningxia Huizu" "Xinjiang Uygur"
         "HK" "MO" "TW" 
     )
 
