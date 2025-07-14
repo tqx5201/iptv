cc937b3706b3a7e087ba436a49dae7b907507840
天仙婆婆 <tqx5201@163.com>
2025-07-14 16:04:55 +0800

    Update fofa_feiyang.sh

--- jiaoben/fofa_feiyang.sh
+++ jiaoben/fofa_feiyang.sh
@@ -2,6 +2,7 @@
 #设置时区
 export TZ=Asia/Shanghai
 ip_file="ip/feiyang.txt"
+rm -rf ip/feiyang.txt
 base_url="https://fofa.info/result?qbase64="
 query='""请求成功，当前ALLINONE版本构建时间为"'
 url_fofa=$(echo -n "$query" | base64 | tr -d '\n')
